a8a7d75fbf2cbb7589eea6c2000fd83d0135b85d
= <=>
2025-06-03 11:24:30 -0400

    fixing the format

--- init.lua
+++ init.lua
@@ -4,7 +4,14 @@ require 'core.snippets'
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    lazyrepo,
+    lazypath,
+  }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
@@ -39,8 +46,10 @@ require('lazy').setup({
   --require 'plugins.kanagawa',
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    -- If you are using a Nerd Font: set icons
+    -- to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons,
+    -- otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
