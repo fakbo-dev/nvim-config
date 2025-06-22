27c22e96e6340560fb5d13a76e47204520035069
fakbo-dev <fakbodev@gmail.com>
2025-06-22 19:21:22 -0400

    try to add images in neovim

--- init.lua
+++ init.lua
@@ -48,6 +48,7 @@ require('lazy').setup({
   require 'plugins.vim-be-good',
   require 'plugins.harpoon',
   require 'plugins.leetcode',
+  require 'plugins.img-clip',
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
