c7f6f5a6f9587b9a2a2f54d56b791405bf695b3c
fakbodev <fakbodev@gmail.com>
2025-06-26 12:13:12 -0400

    something

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -59,3 +59,4 @@ vim.opt.shortmess:append 'c'
 vim.opt.iskeyword:append '-'
 vim.opt.formatoptions:remove { 'c', 'r', 'o' }
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles'
+vim.opt.guicursor = 'n-v-i-c:block-Cursor'
