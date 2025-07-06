d004ba5b352ba0868b0351c3cffb692c7b1de491
fakbo-dev <fakbodev@gmail.com>
2025-07-06 08:49:33 -0400

    it works but need a few changes

--- lua/plugins/zenbones.lua
+++ lua/plugins/zenbones.lua
@@ -1,15 +1,17 @@
 return {
   'zenbones-theme/zenbones.nvim',
-  -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-  -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  -- In Vim, compat mode is turned on as Lush only works in Neovim.
   dependencies = 'rktjmp/lush.nvim',
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.darkness = 'warm'
     vim.g.zenbones_darken_comments = 45
+
     vim.cmd.colorscheme 'rosebones'
-    vim.g.transparent_background = 'true'
+    vim.api.nvim_set_hl(0, 'Normal', { bg = '#000000' })
+
+    vim.api.nvim_set_hl(0, 'LineNr', { bg = '#000000' })
+    vim.api.nvim_set_hl(0, 'FoldColumn', { bg = '#000000' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = '#000000' })
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = '#000000' })
   end,
 }
