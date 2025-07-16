75155dde00073a18cca5841bd15fb7ecad0ff78e
fakbodev <fakbodev@gmail.com>
2025-07-16 08:52:11 -0400

    feat: need to check the gitsings thing and adding validation flag for the dirbuf plugin

--- lua/plugins/dirbuf.lua
+++ lua/plugins/dirbuf.lua
@@ -1,1 +1,15 @@
-return { 'elihunter173/dirbuf.nvim' }
+return {
+  'elihunter173/dirbuf.nvim',
+  config = function()
+    require('dirbuf').setup {}
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'dirbuf',
+      callback = function()
+        vim.keymap.set('n', '<C-s>', function()
+          vim.cmd 'DirbufSync -confirm'
+        end, { buffer = true, desc = 'Dirbuf: Sync changes with confirmation' })
+      end,
+    })
+  end,
+}
