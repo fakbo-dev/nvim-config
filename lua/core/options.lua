8d63520d0f7bb56beefbd73a8b93fe7c0d8f5079
fakbo-dev <fakbodev@gmail.com>
2025-08-25 22:10:34 -0400

    feat: adding 4 spaces indent

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,9 +1,9 @@
 -- Editor settings
 vim.deprecate = function() end
-vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.o.winborder = "rounded"
 
 vim.wo.number = true
