72dfd7fe679b86a84662a01baa974f89cfc5b1c4
fakbodev <fakbodev@gmail.com>
2025-08-27 07:54:51 -0400

    feat: deleting the comments

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,9 +1,9 @@
 -- Editor settings
 vim.deprecate = function() end
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.winborder = "rounded"
 
 vim.wo.number = true
