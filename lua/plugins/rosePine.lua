6cb5fd8667b2d4162f3f88133124e67102ea3e33
fakbo-dev <fakbodev@gmail.com>
2025-09-11 19:37:57 -0400

    yeah

--- lua/plugins/rosePine.lua
+++ lua/plugins/rosePine.lua
@@ -4,15 +4,15 @@ return
   name = "rose-pine",
   config = function()
     require("rose-pine").setup({
-        variant = "auto", -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
+        variant = "auto",
+        dark_variant = "main",
         dim_inactive_windows = false,
         extend_background_behind_borders = true,
 
         enable = {
             terminal = true,
-            legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-            migrations = true, -- Handle deprecated options automatically
+            legacy_highlights = true,
+            migrations = true,
         },
 
         styles = {
@@ -53,36 +53,20 @@ return
         },
 
         palette = {
-            -- Override the builtin palette per variant
             -- moon = {
             --     base = '#18191a',
             --     overlay = '#363738',
             -- },
         },
 
-        -- NOTE: Highlight groups are extended (merged) by default. Disable this
-        -- per group via `inherit = false`
         highlight_groups = {
             -- Comment = { fg = "foam" },
             -- StatusLine = { fg = "love", bg = "love", blend = 15 },
             -- VertSplit = { fg = "muted", bg = "muted" },
             -- Visual = { fg = "base", bg = "text", inherit = false },
         },
-
-        before_highlight = function(group, highlight, palette)
-            -- Disable all undercurls
-            -- if highlight.undercurl then
-            --     highlight.undercurl = false
-            -- end
-            --
-            -- Change palette colour
-            -- if highlight.fg == palette.pine then
-            --     highlight.fg = palette.foam
-            -- end
-        end,
     })
 
-    vim.cmd("colorscheme rose-pine")
     vim.cmd("colorscheme rose-pine")
   end
 }
