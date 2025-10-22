f64e88c5b56238636af4fe632abb525b7ae7fff3
fakbodev <fakbodev@gmail.com>
2025-10-22 16:32:10 -0400

    ok

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -75,12 +75,12 @@ return {
       })
     end
   },
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-  },
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   lazy = false,
+  --   dependencies = { "MunifTanjim/nui.nvim" },
+  --   opts = {},
+  -- },
   {
     "luckasRanarison/nvim-devdocs",
     dependencies = {
