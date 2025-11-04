407495938ae95e9937e65a3d7e8308e1f9bebe57
fakbodev <fakbodev@gmail.com>
2025-11-04 15:35:54 -0400

    adding formatting finally

--- init.lua
+++ init.lua
@@ -1,60 +1,59 @@
 vim.opt.termguicolors = true
-require 'core.options'
-require 'core.keymaps'
-require 'core.snippets'
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+require("core.options")
+require("core.keymaps")
+require("core.snippets")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system
-  {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    '--branch=stable',
-    lazyrepo,
-    lazypath,
-  }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  require 'plugins.treesitter',
-  require 'plugins.telescope',
-  require 'plugins.lsp',
-  require 'plugins.autocompletion',
-  require 'plugins.no-ls',
-  require 'plugins.gitsigns',
-  require 'plugins.indent-blankline',
-  require 'plugins.misc',
-  require 'plugins.mason-tempo',
-  require 'plugins.obsidian',
-  require 'plugins.harpoon',
-  require 'plugins.leetcode',
-  require 'plugins.oil',
-  require 'plugins.lualine',
-  require 'plugins.rosePine',
-  require 'plugins.markview',
-  require 'plugins.csvview',
-  require 'plugins.lackluster',
+require("lazy").setup({
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.no-ls"),
+	require("plugins.gitsigns"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.mason-tempo"),
+	require("plugins.obsidian"),
+	require("plugins.harpoon"),
+	require("plugins.leetcode"),
+	require("plugins.oil"),
+	require("plugins.lualine"),
+	require("plugins.rosePine"),
+	require("plugins.markview"),
+	require("plugins.csvview"),
+	require("plugins.lackluster"),
 }, {
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+	ui = {
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
--- lua/plugins/no-ls.lua
+++ lua/plugins/no-ls.lua
@@ -1,51 +1,45 @@
 return {
-  'nvimtools/none-ls.nvim',
-  dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim',
-  },
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
+    local conform = require('conform')
 
-    require('mason-null-ls').setup {
-      ensure_installed = {
-        'prettier',
-        'stylua',
-        'eslint_d',
-        'shfmt',
-        'ruff',
-        'sql-formatter',
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { 'clang-format' },
+        typescript = { 'clang-format' },
+        javascriptreact = { 'clang-format' },
+        typescriptreact = { 'clang-format' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
+        java = { 'clang-format' },
+        json = { 'clang-format' },
+        html = { 'prettier' },
+        css = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        sh = { 'shfmt' },
+        bash = { 'shfmt' },
+        terraform = { 'terraform_fmt' },
+        python = { 'ruff_format', 'ruff_organize_imports' },
+        lua = { 'stylua' },
       },
-      automatic_installation = true,
-    }
+      
+      formatters = {
+        ['clang-format'] = {
+          prepend_args = { '--style={BasedOnStyle: LLVM, BreakBeforeBraces: Allman}' },
+        },
+        shfmt = {
+          prepend_args = { '-i', '4' },
+        },
+      },
 
-    local sources = {
-      diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'Bash', 'Shell' } },
-      -- formatting.stylua,
-      formatting.shfmt.with { args = { '-i', '4' } },
-      formatting.terraform_fmt,
-      require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-      require 'none-ls.formatting.ruff_format',
-    }
-
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-    null_ls.setup {
-      sources = sources,
-      on_attach = function(client, bufnr)
-        if client.server_capabilities.documentFormattingProvider then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-        end
-      end,
-    }
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
   end,
 }
+
