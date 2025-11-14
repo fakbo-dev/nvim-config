ca024936988ea9ac28ce16c716ee29e2eaa850cd
fakbodev <fakbodev@gmail.com>
2025-11-14 09:12:02 -0400

    adding styluao dont like it

--- lua/plugins/lackluster.lua
+++ lua/plugins/lackluster.lua
@@ -1,51 +1,47 @@
 return {
-    -- testing
-    'slugbyte/lackluster.nvim',
-    lazy = false,
-    priority = 1000,
+	-- testing
+	"slugbyte/lackluster.nvim",
+	lazy = false,
+	priority = 1000,
 
-    config = function()
-
-        require('lackluster').setup({
-            tweak_background=
-            {
-                normal = 'none',
-                telescope = 'none',
-            },
-            tweak_syntax =
-            {
-                string = '#988069',
-                comment = '#999DA0'
-            },
-            disable_plugin =
-            {
-                bufferline = false,
-                cmp = false,
-                dashboard = false,
-                flash = false,
-                git_gutter = false,
-                git_signs = false,
-                headline = false,
-                indentmini = false,
-                lazy = false,
-                lightbulb = false,
-                lsp_config = false,
-                mason = false,
-                mini_diff = false,
-                navic = false,
-                noice = false,
-                notify = false,
-                oil = false,
-                rainbow_delimiter = false,
-                scollbar = false,
-                telescope = false,
-                todo_comments = true,
-                tree = false,
-                trouble = false,
-                which_key = false,
-                yanky = true,
-            },
-        })
-        vim.cmd.colorscheme('lackluster-hack')
-    end
+	config = function()
+		require("lackluster").setup({
+			tweak_background = {
+				normal = "none",
+				telescope = "none",
+			},
+			tweak_syntax = {
+				string = "#988069",
+				comment = "#999DA0",
+			},
+			disable_plugin = {
+				bufferline = false,
+				cmp = false,
+				dashboard = false,
+				flash = false,
+				git_gutter = false,
+				git_signs = false,
+				headline = false,
+				indentmini = false,
+				lazy = false,
+				lightbulb = false,
+				lsp_config = false,
+				mason = false,
+				mini_diff = false,
+				navic = false,
+				noice = false,
+				notify = false,
+				oil = false,
+				rainbow_delimiter = false,
+				scollbar = false,
+				telescope = false,
+				todo_comments = true,
+				tree = false,
+				trouble = false,
+				which_key = false,
+				yanky = true,
+			},
+		})
+		vim.cmd.colorscheme("lackluster-hack")
+	end,
 }
--- lua/plugins/no-ls.lua
+++ lua/plugins/no-ls.lua
@@ -1,46 +1,45 @@
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    local conform = require('conform')
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { 'clang-format' },
-        typescript = { 'clang-format' },
-        javascriptreact = { 'clang-format' },
-        typescriptreact = { 'clang-format' },
-        c = { 'clang-format' },
-        cpp = { 'clang-format' },
-        java = { 'clang-format' },
-        json = { 'clang-format' },
-        html = { 'prettier' },
-        css = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
-        sh = { 'shfmt' },
-        bash = { 'shfmt' },
-        terraform = { 'terraform_fmt' },
-        python = { 'ruff_format', 'ruff_organize_imports' },
-        -- lua = { 'stylua' },
-        sql = {'sql_formatter'},
-      },
-      
-      formatters = {
-        ['clang-format'] = {
-          prepend_args = { '--style={BasedOnStyle: webkit, BreakBeforeBraces: Allman}' },
-        },
-        shfmt = {
-          prepend_args = { '-i', '4' },
-        },
-      },
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "clang-format" },
+				typescript = { "clang-format" },
+				javascriptreact = { "clang-format" },
+				typescriptreact = { "clang-format" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				java = { "clang-format" },
+				json = { "clang-format" },
+				html = { "prettier" },
+				css = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				sh = { "shfmt" },
+				bash = { "shfmt" },
+				terraform = { "terraform_fmt" },
+				python = { "ruff_format", "ruff_organize_imports" },
+				lua = { "stylua" },
+				sql = { "sql_formatter" },
+			},
 
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
-  end,
+			formatters = {
+				["clang-format"] = {
+					prepend_args = { "--style={BasedOnStyle: webkit, BreakBeforeBraces: Allman}" },
+				},
+				shfmt = {
+					prepend_args = { "-i", "4" },
+				},
+			},
+
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+	end,
 }
-
