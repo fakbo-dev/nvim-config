ef76efc92aa6e07ca85e54fd3ed3af2259c4659b
fakbodev <fakbodev@gmail.com>
2025-12-09 10:56:08 -0400

    adding hardtime once again

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,22 +1,17 @@
-return
-{
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-    config = function ()
-        require('render-markdown').setup({
-            code =
-            {
-                disable_background = true
-            },
-            heading =
-            {
-                -- width = 'block'
-            }
-        })
-
-
-    end
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+	config = function()
+		require("render-markdown").setup({
+			code = {
+				disable_background = true,
+			},
+			heading = {
+				-- width = 'block'
+			},
+		})
+	end,
 }
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,59 +1,64 @@
 return {
-  {
-    'christoomey/vim-tmux-navigator',
-  },
-  {
-    'tpope/vim-sleuth',
-  },
-  {
-    'tpope/vim-rhubarb',
-    dependencies = {'tpope/vim-fugitive'},
-  },
-  {
-    'folke/todo-comments.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      signs = true,
-    TODO = { icon = " ", color = "info" },
-    HACK = { icon = " ", color = "warning" },
-    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-    colors = {
-        error = {  "#DC2626" },
-        warning = {"#FBBF24" },
-        WARN = {"#FFA500"},
-        info = {"#2563EB" },
-        hint = { "#10B981"},
-        default = {"#7C3AED" },
-        test = { "#FF00FF" }
-        },
-    },
-
-  },
-  {
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end,
-  },
-  {
-    "ecthelionvi/NeoColumn.nvim",
-    opts = {always_on = true, bg_color = '#808080', fg_color = '#ffffff'},
-  },
-  {
-    'tronikelis/ts-autotag.nvim',
-    opts = {},
-    event = 'VeryLazy',
-  },
-  {
-    "luckasRanarison/nvim-devdocs",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {}
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+	},
+	{
+		"tpope/vim-sleuth",
+	},
+	{
+		"tpope/vim-rhubarb",
+		dependencies = { "tpope/vim-fugitive" },
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			signs = true,
+			TODO = { icon = " ", color = "info" },
+			HACK = { icon = " ", color = "warning" },
+			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+			TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			colors = {
+				error = { "#DC2626" },
+				warning = { "#FBBF24" },
+				WARN = { "#FFA500" },
+				info = { "#2563EB" },
+				hint = { "#10B981" },
+				default = { "#7C3AED" },
+				test = { "#FF00FF" },
+			},
+		},
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+	{
+		"ecthelionvi/NeoColumn.nvim",
+		opts = { always_on = true, bg_color = "#808080", fg_color = "#ffffff" },
+	},
+	{
+		"tronikelis/ts-autotag.nvim",
+		opts = {},
+		event = "VeryLazy",
+	},
+	{
+		"luckasRanarison/nvim-devdocs",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {},
+	},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
 }
